2661af0d95411ad5f05ba1fde63aff8a73677824
Azhar Ali <azharofficial599@gmail.com>
2021-12-24 00:22:35 +0530

    Updates

---
+++ precache-manifest.cfa3726d8e62c0f250a0a902a172d96a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bf3ad661c7740144febc58d1460a58",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/index.html"
+  },
+  {
+    "revision": "072e0b65331d0a3add22",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "15f893edcd6bde0510da",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/css/main.35b5b4d0.chunk.css"
+  },
+  {
+    "revision": "88666937646067b89483",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/2.3153b7bc.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/2.3153b7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072e0b65331d0a3add22",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/3.5bc25179.chunk.js"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "15f893edcd6bde0510da",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/main.0054339c.chunk.js"
+  },
+  {
+    "revision": "0876ff0ce61c704195e8",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/js/runtime-main.79285f86.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8e109d50e16c6b785bb76fd8c597cec8",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/gosacorp.8e109d50.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3164beaf0db1529f0c11b1c42b0bc3e4",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/mcrp.3164beaf.jpeg"
+  },
+  {
+    "revision": "a44c8e1634562d745af53cd088a55a5d",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/profound-logo.a44c8e16.png"
+  },
+  {
+    "revision": "3dff18194285fd36755c05518486944f",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/py.3dff1819.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/AzharOfficialGitAccount/AzharPortfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
